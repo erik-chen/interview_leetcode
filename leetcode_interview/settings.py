ab49d51bea24bb7b471f4bc68487bbb74e096220
erik-chen <zhihaochen10@163.com>
2020-02-16 20:51:41 +0800

    Update settings.py

--- leetcode_interview/settings.py
+++ leetcode_interview/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'interview'
+    'interview',
+    'graphene_django',
 ]
 
 MIDDLEWARE = [
